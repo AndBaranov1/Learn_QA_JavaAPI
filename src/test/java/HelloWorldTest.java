1dd0f698a24f36a2b88af9f89f57043e9a8ce718
Andrey_Baranov <37299544+AndBaranov1@users.noreply.github.com>
2025-05-13 12:46:16 +0700

    test-example4

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,5 +1,5 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -10,15 +10,21 @@ public class HelloWorldTest {
 
     @Test
     public void testRestAssured() {
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader1", "myValue1");
+        headers.put("myHeader2", "myValue2");
         Response response = RestAssured
                 .given()
                 .redirects()
-                .follow(true)
+                .follow(false)
                 .when()
                 .get(" https://playground.learnqa.ru/api/get_303")
                 .andReturn();
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
+        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
     }
 
     @Test
