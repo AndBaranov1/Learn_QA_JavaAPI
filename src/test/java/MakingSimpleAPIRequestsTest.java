bf147d2813ef28df35c41b1885cc48835e1656c9
Andrey_Baranov <37299544+AndBaranov1@users.noreply.github.com>
2025-05-14 17:36:16 +0700

    Ex6: Редирект

--- src/test/java/MakingSimpleAPIRequestsTest.java
+++ src/test/java/MakingSimpleAPIRequestsTest.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -21,4 +22,22 @@ public class MakingSimpleAPIRequestsTest {
         String secondMessage = response.getString("messages[1].message");
         System.out.println("Второе сообщение: " + secondMessage);
     }
+    @Test
+    public void testRestAssuredRedirect() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader1", "myValue1");
+        headers.put("myHeader2", "myValue2");
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+
 }
