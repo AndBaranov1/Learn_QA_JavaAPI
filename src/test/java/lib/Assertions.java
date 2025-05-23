0962d0595ee0b34c1aa32ed537c125a4f01bc032
Andrey_Baranov <37299544+AndBaranov1@users.noreply.github.com>
2025-05-23 18:38:30 +0700

    01. Создание существующего пользователя

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -14,4 +14,11 @@ public class Assertions {
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
     }
 
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer) {
+        assertEquals(expectedAnswer, Response.asString(), "Response text is not as expected");
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode) {
+        assertEquals(expectedStatusCode, Response.statusCode(), "Response code is not as expected");
+    }
 }
